db81c192386eba85b2d753dc08b9975b3e5089e9
Blackston <blackston858@gmail.com>
2026-01-06 15:21:50 +0500

    New update1.2

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,27 +11,86 @@ const Cards = () => {
       <div className="flex justify-between p-10  items-center">
         <h1 className="text-white font-medium ">Latest Personal Care</h1>
 
-        <button className="bg-white px-3 py-2 text-black ">View All</button>
+        <button className="bg-white px-3 py-2 text-black bg-image">View All</button>
       </div>
 
       <div>
-        <div className="slider-container overflow-hidden grid-flow-row grid-rows-2 grid-cols-4 gap-4">
-          <Slider className="gap-5"  >
+         
+          <Slider className=""  >
              <div className="slide">
-                <div className="grid-cols-4, grid-rows-2, gap-4">
+                <div className="grid grid-cols-4 grid-rows-2 gap-4  max-w-7xl mx-auto   "  >
 
-                    <div>
-                        <img className="w-[50px] h-[50px]" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
-                        <p className="bg-[#fbf7eb] flex"><FaStar className="text-yellow-400" />5.0 <span>5 Review</span></p>
+                   <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
                      </div>
- 
+
+                     <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+
+                      <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+
+                      <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+
+
+                      <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+                       <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+                      <div className="    bg-white items-center rounded-lg p-5  pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+
+                      <div className="    bg-white items-center rounded-lg p-5 space-y2 pb-4 ">
+                        <img className="w-[50px] h-[50px] flex justify-center mt-3" src="https://images.priceoye.pk/dingling-rf-609-rechargeable-professional-hair-beard-trimmer-pakistan-priceoye-pfhd3-270x270.webp" alt=""/>
+                        <p className="bg-[#fbf7eb] flex items-center gap-1 mt-3"><FaStar className="text-yellow-400" />5.0 <span className="text-[10px]">5 Review</span></p>
+                        <h1 className="opacity-75  mt-3">Dingling RF-609 Rechargeable Professional Hair & Beard...</h1>
+                        <h1 className="text-[16px] mt-3"><span className="text-[16px] "   >Rs</span>2,699 <span className="text-[10px] text-green-600">40% off</span></h1>
+                        <p className="font-medium text-[12px] mt-3  rounded-xl text[10px] w-10 text-center  bg-[#f90390] text-white">Fast<span className="bg-[#fff348] ml-2 text-black font-medium text-[12px]  rounded-xl text[10px] w-10">Delivery</span></p>
+                     </div>
+
+                      
 
                 </div>
              </div>
              
              
           </Slider>
-        </div>
+         
       </div>
     </div>
   );
